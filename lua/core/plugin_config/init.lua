75d156809b6829d20867c369d5bda61cc24b2247
JanMichaelSe <janm.montalvo@gmail.com>
2023-03-24 21:15:11 -0400

    added neo vim tree for better file management

--- lua/core/plugin_config/init.lua
+++ lua/core/plugin_config/init.lua
@@ -2,3 +2,4 @@
 require('core.plugin_config.telescope')
 require('core.plugin_config.treesitter')
 require('core.plugin_config.lsp')
+require('core.plugin_config.neo-tree')
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,15 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function ()
+      require('neo-tree').setup {}
+    end,
+}
