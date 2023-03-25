1c2ec79b3b1edc36c1816665ba7055fae764c758
JanMichaelSe <janm.montalvo@gmail.com>
2023-03-24 20:36:50 -0400

    Organized files for NeoVim configuration

--- init.lua
+++ init.lua
@@ -6,8 +6,8 @@
 
 Important Steps for new device:
   - Setup up Path variables for the following:
-    - Easy way to get back to config when you need too. 
-    - Things like Cmake, Gcc and Clang
+  - Easy way to get back to config when you need too. 
+  - Things like Cmake, Gcc and Clang
 
 Important Resources for Windows Setup
   - https://github.com/nvim-lua/kickstart.nvim
@@ -28,7 +28,7 @@ Important Script to make Opening Folders with NeoVim Easier:
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Install package manager
+-- [[ Install package manager ]]
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -44,7 +44,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- --- INSTALLATION OF PACKAGES START HERE ---
 -- NOTE: Here is where you install your plugins.
 --  You can configure plugins using the `config` key.
 --
@@ -158,42 +157,11 @@ require('lazy').setup({
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
   },
-
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
-
-  -- NOTE: The import below automatically adds your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  --
-  --    An additional note is that if you only copied in the `init.lua`, you can just comment this line
-  --    to get rid of the warning telling you that there are not plugins in `lua/custom/plugins/`.
   { import = 'custom.plugins' },
 }, {})
 
-require('core.set')
-
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
-
--- PLUGIN CONFIG --
-require('core.plugin_config')
-
--- Custom Mappings ---
-require('core.keymaps')
+-- [[ Require Core Configuration ]]
+require('core')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
