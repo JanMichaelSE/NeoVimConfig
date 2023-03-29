345d0f37fda089ac797bfd6cfd42158fd89006c4
JanMichaelSe <janm.montalvo@gmail.com>
2023-03-29 19:51:47 -0400

    Added some usefull remaps and used the same api to modify remaps.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -15,15 +15,20 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagn
 
 -- [[ Custom Keymaps ]]
 -- Move vertically while also centering
-vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true })
+vim.keymap.set('n', 'n', 'nzz', { noremap = true })
+vim.keymap.set('n', 'N', 'Nzz', { noremap = true })
 
 -- Move selection up and down with ease
-vim.api.nvim_set_keymap('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true })
-vim.api.nvim_set_keymap('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true })
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true })
 
 -- Use normal Ctrl+Backspace to erase words easily while writing
-vim.api.nvim_set_keymap('i', '<C-BS>', '<C-o>diw', {})
+vim.keymap.set('i', '<C-BS>', '<C-o>diw', {})
 
 -- Map Ctrl + a to yank the whole buffer to the clipboard
-vim.api.nvim_set_keymap('n', '<C-a>', ':%y+<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-a>', ':%y+<CR>', { noremap = true, silent = true })
+
+-- Paste without replacing system clipboard
+vim.keymap.set('n', '<leader>p', '"_dP', { silent = true })
