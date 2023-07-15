f244b72ad4688b24a289c7cd31fda2da43c52d60
janmichaelse <janm.montalvo@gmail.com>
2023-07-15 00:48:16 -0400

    Started linux configuration

--- init.lua
+++ init.lua
@@ -156,7 +156,7 @@ require('lazy').setup({
     -- NOTE: If you are having trouble with this installation,
     --       refer to the README for telescope-fzf-native for more instructions.
     build =
-    'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+    'make',
     cond = function()
       return vim.fn.executable 'make' == 1
     end,
